de39586599f4431ef9aaf4f9682a6dde310e537a
MykolaZubko <kolkazubko@gmail.com>
2021-10-29 16:07:30 +0300

    Update checkbox.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 $(function(){
-
+    
+    $('.carousel__item-list').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: 'carousel__item'
+      });
+      $('.carousel__item').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.carousel__item',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true
+      });
+      
 });
